9473ce20c9567b984e6290a62d24484a3f6d5280
ShamrockD <sheogoraths_joke@mail.ru>
2021-06-23 22:04:53 +0200

    Updated main
    Added pagination

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-import lxml
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+from config import DRIVER_PATH, URL
 
 url = input()
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:89.0) Gecko/20100101 Firefox/89.0'}
+fake_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:89.0) Gecko/20100101 Firefox/89.0'}
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=fake_headers)
 check_response_code = response.status_code
-
 print(check_response_code)
+response = response.text
+
+# Узнаем количество страниц
+
+def get_pages_count():
+
+    bs4_soup = BeautifulSoup(response, 'lxml')
+
+    pages_group = bs4_soup.find('span', 'bloko-button-group')
+    pagination_spans = pages_group.find('a', 'bloko-button')
+    pages_count = pagination_spans.find_next().get_text()
+
+    print(f'Всего страниц для поиска {pages_count}')
+    return pages_count
+
+def
+
+
+if check_response_code == 200:
+    get_pages_count()
